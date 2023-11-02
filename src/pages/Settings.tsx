bd850dbc1fdc54376416681d0530fa5d4fdb93da
Michael Wang <michael19920327@gmail.com>
2023-11-02 20:53:13 +0800

    feat: add settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,93 @@
-export function Settings() {
+import { useState, useRef, useEffect } from 'react';
+import MonacoEditor from '@monaco-editor/react';
+
+import SETTINGS_JSON from '@/../src-tauri/src/app/settings.json';
+import { 
+  debounce,
+  getSettingsPath,
+  readSettings,
+  writeSettings,
+} from '@/utils';
+
+interface EditorProps {
+  lang?: string;
+  defaultValue?: string;
+  onChange?: (content?: string) => void;
+}
+
+function Editor ({
+  lang = 'json',
+  defaultValue = '',
+  onChange,
+}: EditorProps) {
+  const [content, setContent] = useState('');
+  useEffect(() => {
+    setContent(defaultValue);
+    onChange && onChange(defaultValue);
+  }, [defaultValue, onChange]);
+
+  const handleChange = (value: string = '') => {
+    onChange && onChange(value);
+    setContent(value);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-3xl font-bold">Settings</h1>
-      <p className="text-xl font-medium">Coming soon...</p>
+    <div className="w-full h-screen">
+      <MonacoEditor
+        defaultLanguage={lang}
+        value={content}
+        onChange={handleChange}
+      />
     </div>
+  );
+}
+
+const SETTING_JSON_STRING = JSON.stringify(SETTINGS_JSON, null, 4);
+
+export function Settings() {
+  // const navigate = useNavigate();
+  const isInit = useRef(true);
+  const [content, setContent] = useState('');
+  const [, setFilePath] = useState('');
+
+  useEffect(() => {
+    return () => {
+      if (!isInit.current) {
+        window.location.reload();
+        return;
+      }
+      isInit.current = false;
+    };
+  }, []);
+
+  const writeContent = async (value?: string) => {
+    const _data = value || SETTING_JSON_STRING;
+    writeSettings(_data);
+  };
+
+  const handleEdit = debounce(writeContent, 500);
+
+  useEffect(() => {
+    (async () => {
+      setFilePath(await getSettingsPath());
+      const _content = await readSettings();
+      if (!_content) {
+        writeContent(SETTING_JSON_STRING);
+        setContent(SETTING_JSON_STRING);
+        return;
+      }
+      setContent(_content);
+    })();
+  }, []);
+
+  // const handleScript = () => {
+  //   navigate('/script');
+  // };
+
+  return (
+    <Editor
+      defaultValue={content}
+      onChange={handleEdit}
+    />
   );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,37 @@
+import { homeDir, join } from '@tauri-apps/api/path';
+import { readTextFile, writeTextFile } from '@tauri-apps/api/fs';
+
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function debounce<Params extends unknown[]>(
+  fn: (...args: Params) => unknown,
+  timeout: number,
+): (...args: Params) => void {
+  let timer: NodeJS.Timeout;
+  return (...args: Params) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, timeout);
+  };
+}
+
+
+export async function getSettingsPath() {
+  const home = await homeDir();
+  return join(home, '.hackmd', 'settings.json');
+}
+
+export async function readSettings() {
+  return await readTextFile(await getSettingsPath());
+}
+
+export async function writeSettings(content: string) {
+  const setting = await getSettingsPath();
+  await writeTextFile(await join(setting), content);
 }
