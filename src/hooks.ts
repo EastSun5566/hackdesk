7da517c17cc414242249dc3c7dd7c7a0f73aab0c
Michael Wang <michael19920327@gmail.com>
2024-10-13 19:59:09 +0800

    feat: install `@hackmd/api`

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,17 @@
 import { useState, useEffect, useContext } from 'react';
+import { useQuery } from 'react-query';
+import HackMDApi from '@hackmd/api';
+
 import DEFAULT_SETTINGS from '@/../src-tauri/src/app/settings.json';
 import { ThemeContext } from './components/theme-provider';
 import { readSettings, writeSettings } from './utils';
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) throw new Error('useTheme must be used within a ThemeProvider');
+
+  return context;
+}
 
 const DEFAULT_SETTING_STRING = JSON.stringify(DEFAULT_SETTINGS, null, 2);
 export function useSettings() {
@@ -21,9 +31,19 @@ export function useSettings() {
   return { settings , setSettings };
 }
 
-export function useTheme() {
-  const context = useContext(ThemeContext);
-  if (context === undefined) throw new Error('useTheme must be used within a ThemeProvider');
+let hackMDApiClient: HackMDApi | null = null;
+export function useHackMDApiClient() {
+  const { settings } = useSettings();
+  if (hackMDApiClient) return hackMDApiClient;
 
-  return context;
+  const { hackMDApiAccessToken } = JSON.parse(settings || '{}') as Partial<typeof DEFAULT_SETTINGS>;
+  if(!hackMDApiAccessToken) return null;
+  
+  hackMDApiClient = new HackMDApi(hackMDApiAccessToken);
+  return hackMDApiClient;
+}
+
+export function useHistory() {
+  const hackMDApiClient = useHackMDApiClient();
+  return useQuery('history', () => hackMDApiClient?.getHistory());
 }
