125e3341a3df1acc96134ef823e30e56b7a26677
Michael Wang <michael19920327@gmail.com>
2026-01-13 22:02:57 +0800

    fix: sync version

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,29 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod app;
+mod utils;
+
+use app::{cmd, menu, setup};
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![
+            cmd::open_command_palette_window,
+            cmd::open_settings_window,
+            cmd::run_script,
+            cmd::apply_settings,
+            // because window.open not working {@link https://github.com/tauri-apps/wry/issues/649}
+            cmd::open_link
+        ])
+        .setup(setup::init)
+        .menu(menu::init)
+        .on_menu_event(menu::handler)
+        .run(tauri::generate_context!())
+        .expect("error while running HackDesk application");
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod app;
-mod utils;
-
-use app::{cmd, menu, setup};
-
-#[tokio::main]
-async fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![
-            cmd::open_command_palette_window,
-            cmd::open_settings_window,
-            cmd::run_script,
-            cmd::apply_settings,
-            // because window.open not working {@link https://github.com/tauri-apps/wry/issues/649}
-            cmd::open_link
-        ])
-        .setup(setup::init)
-        .menu(menu::init)
-        .on_menu_event(menu::handler)
-        .run(tauri::generate_context!())
-        .expect("error while running HackDesk application");
+fn main() {
+    hackdesk::run();
 }
