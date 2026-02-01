d51fdf84ca0229e07ae6ed9fd92b931c8cf19ed6
Michael Wang <michael19920327@gmail.com>
2026-02-01 19:58:43 +0800

    fix: build tray

--- src-tauri/src/app/tray.rs
+++ src-tauri/src/app/tray.rs
@@ -3,13 +3,19 @@ use tauri::{
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     AppHandle, Manager,
 };
+use tracing::info;
 
 pub fn init(app: &tauri::AppHandle) -> tauri::Result<()> {
+    info!("Creating tray menu");
+
     let quit = MenuItemBuilder::with_id("quit", "Quit").build(app)?;
     let menu = Menu::with_items(app, &[&quit])?;
 
+    info!("Building tray icon");
+
     let tray_builder = TrayIconBuilder::new()
         .menu(&menu)
+        .icon(app.default_window_icon().unwrap().clone())
         .on_menu_event(handler)
         .on_tray_icon_event(|tray, event| {
             if let TrayIconEvent::Click {
@@ -26,13 +32,21 @@ pub fn init(app: &tauri::AppHandle) -> tauri::Result<()> {
             }
         });
 
-    let _tray = tray_builder.build(app)?;
+    let tray = tray_builder.build(app)?;
+
+    // Keep the tray icon alive by storing it in app state
+    app.manage(tray);
+
+    info!("Tray icon created successfully");
 
     Ok(())
 }
 
 fn handler(app: &AppHandle, event: tauri::menu::MenuEvent) {
+    info!("Tray menu event: {}", event.id().as_ref());
+
     if event.id().as_ref() == "quit" {
+        info!("Quitting application from tray");
         app.exit(0);
     }
 }
