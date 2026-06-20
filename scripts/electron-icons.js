9b1a88555261969733812f6bcbd98355d61f8e9b
Michael Wang <michael19920327@gmail.com>
2026-06-21 06:07:18 +0800

    ci: fix build script on win

--- scripts/electron-icons.js
+++ scripts/electron-icons.js
@@ -10,18 +10,32 @@ const rootDir = join(__dirname, '..');
 const sourceIcon = join(rootDir, 'docs/public/logo.png');
 const buildDir = join(rootDir, 'build');
 const tempDir = mkdtempSync(join(tmpdir(), 'hackdesk-electron-icons-'));
+const pnpmArgs = ['exec', 'tauri', 'icon', sourceIcon, '-o', tempDir];
+const pnpmExecPath = process.env.npm_config_user_agent?.startsWith('pnpm') ? process.env.npm_execpath : undefined;
+const commandOptions = { cwd: rootDir, stdio: 'inherit' };
+
+function quoteCmdArg(arg) {
+  return `"${arg.replaceAll('"', '""')}"`;
+}
+
+function runPnpm(args) {
+  if (pnpmExecPath) {
+    execFileSync(process.execPath, [pnpmExecPath, ...args], commandOptions);
+    return;
+  }
+
+  if (process.platform === 'win32') {
+    execFileSync(process.env.ComSpec ?? 'cmd.exe', ['/d', '/s', '/c', ['pnpm', ...args].map(quoteCmdArg).join(' ')], commandOptions);
+    return;
+  }
+
+  execFileSync('pnpm', args, commandOptions);
+}
 
 mkdirSync(buildDir, { recursive: true });
 
 try {
-  execFileSync('pnpm', [
-    'exec',
-    'tauri',
-    'icon',
-    sourceIcon,
-    '-o',
-    tempDir,
-  ], { cwd: rootDir, stdio: 'inherit' });
+  runPnpm(pnpmArgs);
 
   for (const fileName of ['icon.png', 'icon.icns', 'icon.ico']) {
     copyFileSync(join(tempDir, fileName), join(buildDir, fileName));
