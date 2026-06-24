fd2f0b7585b0b53093503b93d0fa074154417f2e
Michael Wang <michael19920327@gmail.com>
2026-06-25 07:50:17 +0800

    test: add polyfill prevent error

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -14,6 +14,45 @@ Object.defineProperty(HTMLElement.prototype, 'scrollIntoView', {
   value: vi.fn(),
 });
 
+const createEmptyDomRect = (): DOMRect => {
+  if (typeof DOMRect === 'function') {
+    return new DOMRect(0, 0, 0, 0);
+  }
+
+  return {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    toJSON: () => ({}),
+  } as DOMRect;
+};
+
+const createEmptyDomRectList = (): DOMRectList =>
+  ({
+    length: 0,
+    item: () => null,
+    [Symbol.iterator]: function* () {
+      yield* [];
+    },
+  }) as DOMRectList;
+
+if (typeof Range !== 'undefined') {
+  Object.defineProperty(Range.prototype, 'getBoundingClientRect', {
+    configurable: true,
+    value: createEmptyDomRect,
+  });
+
+  Object.defineProperty(Range.prototype, 'getClientRects', {
+    configurable: true,
+    value: createEmptyDomRectList,
+  });
+}
+
 // Mock matchMedia (required by theme-provider)
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
